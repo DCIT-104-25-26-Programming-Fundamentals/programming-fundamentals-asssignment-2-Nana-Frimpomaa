b3c3ad361371c79298342bd33cfafe80ed548d35
Nana-Frimpomaa <rnefrimpomaa001@st.ug.edu.gh>
2026-08-02 01:37:49 +0000

    Implement prime checking function
    Added a function to check if a number is prime and updated main to use this function.

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,47 @@
 #include <iostream>
 using namespace std;
 
+// Function to check if a number is prime
+bool isPrime(int num)
+{
+    if (num < 2)
+    {
+        return false;
+    }
+
+    for (int i = 2; i <= num / 2; i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    int number;
+
+    cout << "Enter a number: ";
+
+    // Check if the input is a valid number
+    if (!(cin >> number))
+    {
+        cout << "That is not a number." << endl;
+        return 0;
+    }
+
+    if (isPrime(number))
+    {
+        cout << number << " is a prime number." << endl;
+    }
+    else
+    {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
+
